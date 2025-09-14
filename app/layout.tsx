c8170e9c7004fce622eab6a84153199e3cc268e7
shreyazh <shreyash5183@gmail.com>
2025-09-15 01:14:37 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Psychology Roadmap",
+  description: "created for psychology ppl",
+  generator: "shreyazh",
 }
 
 const geistSans = Geist({
